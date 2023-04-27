73356ddbb00c0fce5ab1ae19b7e2b38045340333
Posei3011 <posei3011@gmail.com>
2023-04-27 12:23:09 -0700

    Tasks 0 and 1

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -7,9 +7,10 @@
 
 int main(void)
 {
-	int zero = 0;
+	int i;
 
-	positive_or_negative(zero);
+	i = 0;
+	positive_or_negative(i);
 
 	return (0);
 }
--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,24 +1,12 @@
 #include "main.h"
 
-/**
-  *main - Determine if a number is positive, negative or zero.
-  *0 : is the number to be checked
-  *Return: 0 on success
-  */
 void positive_or_negative(int i)
 {
-	if (i < 0)
-	{
-		printf("%d is %s\n", i, "negative");
-	}
-	else if (i > 0)
-	{
-		printf("%d is %s\n", i, "positive");
-	}
-	else
-	{
-		printf("%d is %s\n", i, "zero");
-	}
-	return;
+	if (i > 0)
+		printf("%d is positive\n", i);
+	else if (i < 0)
+		printf("%d is negative\n", i);
 
+	else
+		printf("%d is zero\n", i);
 }
