5ac7d9bb8278e00e1dec8cf5b31dc85e1434b599
Posei3011 <posei3011@gmail.com>
2023-04-28 08:37:15 -0700

    Tasks 6 to 12

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  *print_line - Draws a straught line according to parameter
+  *@n: The number of lines to draw
+  *Return: empty
+  */
+
+void print_line(int n)
+
+{
+
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
