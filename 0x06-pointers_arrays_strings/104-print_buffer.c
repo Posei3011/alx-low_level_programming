6db5fc3109a12c76c11b4a68b482a1fbb694e381
Posei3011 <posei3011@gmail.com>
2023-05-05 19:50:56 -0700

    Task 12

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,44 +1,64 @@
-#include "main.h"
 #include <stdio.h>
+#include <ctype.h>
+
 /**
- * print_buffer - prints a buffer
- *
- * @b: buffer.
- * @size: size of buffer.
- * Return: no return.
- */
+  *print_buffer - prints a buffer
+  *
+  *@b: bufer
+  *@size: size of buffer
+  *Return: no return
+  */
+
 void print_buffer(char *b, int size)
 {
-
-	int j, k, l;
-
 	if (size <= 0)
-		printf("\n");
-	else
 	{
-		for (j = 0; j < size; j += 10)
+		printf("n");
+		return;
+	}
+
+	int i, j;
+
+	for (i = 0; i < size; i += 10)
+	{
+		printf("%08x: ", i);
+
+		for (j = i; j < i + 10; j += 2)
 		{
-			printf("%.8x:", j);
-			for (k = j; k < j + 10; k++)
+			if (j < size)
 			{
-				if (k % 2 == 0)
-					printf(" ");
-				if (k < size)
-					printf("%.2x", *(b + k));
-				else
-					printf(" ");
+				printf("%02x", *(unsigned char *)(b + j));
 			}
-			printf(" ");
-			for (l = j; l < j + 10; l++)
+			else
 			{
-				if (l >= size)
-					break;
-				if (*(b + l) < 32 || *(b + l) > 126)
-					printf("%c", '.');
-				else
-					printf("%c", *(b + l));
+				printf("  ");
 			}
-			printf("\n");
+
+			if (j + 1 < size)
+			{
+				printf("%02x ", *(unsigned char *)(b + j + 1));
+			}
+			else
+			{
+				printf("   ");
+			}
 		}
+
+		printf(" ");
+
+		for (j = i; j < i + 10; j++)
+		{
+			if (j < size)
+			{
+				printf("%c", isprint(*(b + j)) ? *(b + j) : '.');
+			}
+			else
+			{
+				printf(" ");
+			}
+		}
+
+		printf("n");
 	}
 }
+
